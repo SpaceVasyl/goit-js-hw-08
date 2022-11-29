7cbe5330af1309bb9640524f4b8bdee3261cc350
Vasyl Smutko <Curare16071969@gmail.com>
2022-11-30 01:44:07 +0200

    simplelightbox-complete

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,17 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const galleryPhoto = document.querySelector(".gallery");
+const items = galleryItems;
 
+function createGalleryItems(items) {
+    return items.map((item) => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("");
+}
+
+
+galleryPhoto.innerHTML = createGalleryItems(galleryItems);
+const lightbox = new SimpleLightbox(".gallery__item", { captionsData: "alt" , captionDelay: 250} );
 console.log(galleryItems);
