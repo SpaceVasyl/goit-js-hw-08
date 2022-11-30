367e4ce4310fb8f0b307de1f0042e8cc98156872
Vasyl Smutko <Curare16071969@gmail.com>
2022-11-30 02:31:13 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+console.log(iframe);
+    player.on('play', function() {
+        console.log('played the video!');
+    });
 
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+player.getCurrentTime().then(function(seconds) {
+    // seconds = the current playback position
+}).catch(function(error) {
+    // an error occurred
+});
