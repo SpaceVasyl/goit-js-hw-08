a1071eb2140aaa37a12cac60ad5e0929e9d4c070
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2022-12-02 00:59:12 +0000

    Deploying to gh-pages from @ SpaceVasyl/goit-js-hw-08@667fc7cea7f6699218b3a35fd878171498429838 🚀

---
+++ 02-video.51e0f69e.js
@@ -0,0 +1,2 @@
+const n=document.querySelector("iframe"),e=new Vimeo.Player(n);e.on("timeupdate",(function(n){console.log(n)})),e.getCurrentTime().then((function(n){})).catch((function(n){})),e.getCuePoints().then((function(n){})).catch((function(n){n.name,console.log(n)}));
+//# sourceMappingURL=02-video.51e0f69e.js.map
---
+++ 02-video.b20a01bf.js
@@ -0,0 +1,2 @@
+!function(){var n=document.querySelector("iframe"),e=new Vimeo.Player(n);e.on("timeupdate",(function(n){console.log(n)})),e.getCurrentTime().then((function(n){})).catch((function(n){})),e.getCuePoints().then((function(n){})).catch((function(n){n.name,console.log(n)}))}();
+//# sourceMappingURL=02-video.b20a01bf.js.map
