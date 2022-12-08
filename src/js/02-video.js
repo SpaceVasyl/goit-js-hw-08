e2911d4e151b8c085cad7cd9cb283bc1505f8cdf
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-08 18:20:39 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,7 @@
 const iframe = document.querySelector('iframe');
       const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', function (seconds) {
-    
+player.on('timeupdate', function ({ seconds }) {
         console.log(seconds);
       });
 
-      
-
-      player.getCurrentTime().then(function(seconds) {
-}).catch(function(error) {
-});
-
-player.getCuePoints().then(function(cuePoints) {
-    // cuePoints = an array of cue point objects
-}).catch(function(error) {
-    switch (error.name) {
-        case 'UnsupportedError':
-            // cue points are not supported with the current player or browser
-            break;
-
-        default: "videoplayer-current-time"
-            // some other error occurred 
-            break;
-    }
-    console.log(error);
-});
