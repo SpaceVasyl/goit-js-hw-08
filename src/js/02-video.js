5b049a6c448344e100329d2a357cdd74c474d96d
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-10 23:47:08 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,13 @@
+import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', function ({ seconds }) {
-  localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
-  // console.log(theme);
-});
-      
+  player.on('timeupdate', function ({ seconds }) {
+  
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+  });
+
+
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
 }).catch(function(error) {
     switch (error.name) {
