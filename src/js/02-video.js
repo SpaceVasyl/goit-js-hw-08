e58b7f3cbbd9ddc929d467b565691a499108e637
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-10 23:19:05 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,20 @@
-const iframe = document.querySelector('iframe');*6]\cccccc
-      const player = new Vimeo.Player(iframe);
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
 player.on('timeupdate', function ({ seconds }) {
-        console.log(seconds);
+  localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds));
+  // console.log(theme);
 });
       
-function saveMessage(evt) {
-  evt.preventDefault();
-    localStorage.setItem("videoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timevideoplayer-current-timeoooooooo
-    ///////////-  updateOutput();
-  form.reset();
-}
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
