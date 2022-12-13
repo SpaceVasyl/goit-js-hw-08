a8a33fcae237244ec893ab0afda6438330b1f732
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-13 21:29:00 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,6 +8,17 @@ if (localStorage.length != 1) {
   form.email.value = parsedSettings.email;
   form.message.value = parsedSettings.message;
 }
+
+form.addEventListener("input", printInput);
+function printInput(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message }
+  } = event.currentTarget;
+  localStorage.setItem("feedback-form-state", JSON.stringify({email: email.value, message: message.value}))
+}
+
+
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
@@ -15,11 +26,9 @@ function handleSubmit(event) {
   const {
     elements: { email, message }
   } = event.currentTarget;
-  
   if (email.value === "" || message.value === "") {
     return console.log("Please fill in all the fields!");
   }
-  localStorage.setItem("feedback-form-state", JSON.stringify({email: email.value, message: message.value}))
   console.log({email: email.value, message: message.value});
   event.currentTarget.reset();
   localStorage.removeItem("feedback-form-state");
