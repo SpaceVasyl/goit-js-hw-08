2dc9a1fe9583faf2b7917c8038b83d53e25030fe
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-13 16:50:41 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,7 @@
 import throttle from "lodash.throttle";
 import player from "@vimeo/player";
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player('iframe');
+ const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
 
 const videoplayerCurrentTime = 'videoplayer-current-time'
 player.on('timeupdate', function (event) {
