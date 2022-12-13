56ef7b8973f344449dcbdb74bbb3f64c9805b9fa
Vasyl Smutko <Curare16071969@gmail.com>
2022-12-13 20:28:59 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,20 @@
-import throttle from "lodash.throttle";
+// import throttle from "lodash.throttle";
 
-const form = document.querySelector("feedback-form");
-form.addEventListener("input", handleInput);
+const form = document.querySelector(".feedback-form");
 
-function handleInput(event) {
-    event.preventDefault();
-    const {
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
     elements: { email, message }
   } = event.currentTarget;
+
+  if (email.value === "" || message.value === "") {
+    return console.log("Please fill in all the fields!");
+  }
+  localStorage.setItem("feedback-form-state", JSON.stringify({email: email.value, message: message.value}))
+  console.log({email: email.value, message: message.value});
+  event.currentTarget.reset();
 }
+
